faf051219a19a998747d89f849d352dbaaea8389
Joao Esperancinha <jofisaes@gmail.com>
2020-01-07 21:30:45 +0100

    Creates concert application

---
+++ concert-demos-rest-service/src/main/kotlin/org/jesperancinha/concerts/concertdemosroot/ConcertDemosRootApplication.kt
@@ -0,0 +1,11 @@
+package org.jesperancinha.concerts.concertdemosroot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConcertDemosRootApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConcertDemosRootApplication>(*args)
+}
---
+++ concert-demos-rest-service/src/test/kotlin/org/jesperancinha/concerts/concertdemosroot/ConcertDemosRootApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jesperancinha.concerts.concertdemosroot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConcertDemosRootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
