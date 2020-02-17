8668dd6c35b679bf9494ece659242a5029009cb7
Joao Esperancinha <jofisaes@gmail.com>
2020-02-17 21:40:37 +0100

    Postgres image running

--- docker-psql/init-dbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE concertsdb-mvc;
-    CREATE DATABASE concertsdb-webflux;
-EOSQL
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-service postgresql restart
 java -jar -Dspring.profiles.active=dev concert-demos-rest-service-mvc-0.0.1-SNAPSHOT.jar --postgres.host=postgres &
 java -jar -Dspring.profiles.active=dev concert-demos-rest-service-webflux-0.0.1-SNAPSHOT.jar --postgres.host=postgres
