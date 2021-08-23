50f444fbb7500440c315a6882a84a98cb2970428
Joao Esperancinha <jofisaes@gmail.com>
2021-08-23 23:55:42 +0200

    🐳 Webflux and Mvc containers

--- docker-psql/build-standalone.sh
+++ docker-psql/build-standalone.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-docker-machine start dev
-
-docker-machine env dev
-
-eval $(docker-machine env dev)
-
 docker-compose down
 
 docker stop postgres-standalone
@@ -14,4 +8,4 @@ docker rm postgres-standalone
 
 docker build . -t postgres-image
 
-docker run --name postgres-standalone -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=admin -p 5432:5432 -d postgres:12
+docker run --name postgres-standalone -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=admin -e POSTGRES_MULTIPLE_DATABASES=vsa -p 5432:5432 -d postgres-image
