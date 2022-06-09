80fcd65613ce6cd8ee9036ea3abe93d28a32390d
Joao Esperancinha <jofisaes@gmail.com>
2022-06-10 00:33:27 +0200

    Updates to jakarte package

--- concert-demos-rest-service-mvc/src/main/kotlin/org/jesperancinha/concerts/mvc/controllers/ArtistControllerImpl.kt
+++ concert-demos-rest-service-mvc/src/main/kotlin/org/jesperancinha/concerts/mvc/controllers/ArtistControllerImpl.kt
@@ -1,11 +1,11 @@
 package org.jesperancinha.concerts.mvc.controllers
 
+import jakarta.validation.constraints.NotNull
 import org.jesperancinha.concerts.data.ArtistDto
 import org.jesperancinha.concerts.mvc.services.ArtistService
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.validation.constraints.NotNull
 
 @RestController
 @RequestMapping("/concerts/data/artists")
