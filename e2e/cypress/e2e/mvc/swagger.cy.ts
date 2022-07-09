676b75229317cd6730d19333640cb61c2aecf123
Joao Esperancinha <jofisaes@gmail.com>
2022-07-09 23:26:16 +0200

    Tests value of server host

--- e2e/cypress/e2e/mvc/swagger.cy.ts
+++ e2e/cypress/e2e/mvc/swagger.cy.ts
@@ -3,5 +3,7 @@ describe('MVC E2E Tests', () => {
     cy.visit('http://localhost:8080/swagger-ui/index.html');
     cy.get('h2').contains('OpenAPI definition').should('not.be.null');
     cy.wait(1000);
+
+    cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8080');
   });
 })
