a23d60d805bd51597c95a5cc4918bb5b72cf7107
Joao Esperancinha <jofisaes@gmail.com>
2022-12-28 22:07:19 +0100

    More tries

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        retries: {
+            runMode: 4,
+            openMode: 0
+        }
     },
-  },
 });
