a2f549c5aeb81df041f5a62e135556da9a8e0074
jesperancinha <jofisaes@gmail.com>
2024-12-24 16:15:54 +0100

    Adds no args

--- concert-demos-rest-service-mvc/src/main/kotlin/org/jesperancinha/concerts/mvc/daos/Daos.kt
+++ concert-demos-rest-service-mvc/src/main/kotlin/org/jesperancinha/concerts/mvc/daos/Daos.kt
@@ -46,32 +46,8 @@ data class Listing(
     var musics: MutableSet<Music> = HashSet(),
 
     @ManyToMany(mappedBy = "listings")
-    val concerts: MutableSet<Concert> = HashSet(),
-) {
-
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Listing
-
-        if (id != other.id) return false
-        if (artist != other.artist) return false
-        if (referenceMusic != other.referenceMusic) return false
-        if (musics != other.musics) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + artist.hashCode()
-        result = 31 * result + referenceMusic.hashCode()
-        result = 31 * result + musics.hashCode()
-        return result
-    }
-}
+    val concerts: MutableSet<Concert> = HashSet()
+)
 
 @Entity
 data class Music(
