af11c9b54bdbca1f0f0dbbdfd20a6a74647600bf
João Esperancinha <jofisaes@gmail.com>
2025-11-09 15:46:15 +0100

    Simplify element selection in swagger e2e tests.

--- e2e/cypress/e2e/mvc/swagger.cy.ts
+++ e2e/cypress/e2e/mvc/swagger.cy.ts
@@ -2,7 +2,7 @@ describe('MVC E2E Tests', () => {
   it('shows swagger', () => {
     let host = Cypress.env('host_mvc') ? Cypress.env('host_mvc') : 'localhost';
     cy.visit(`http://${host}:8080/swagger-ui/index.html`);
-    cy.get('h2').contains('OpenAPI definition').should('not.be.null');
+    cy.contains('OpenAPI definition').should('not.be.null');
     cy.wait(1000);
 
     cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8080');
--- e2e/cypress/e2e/webflux/swagger.cy.ts
+++ e2e/cypress/e2e/webflux/swagger.cy.ts
@@ -2,7 +2,7 @@ describe('WebFlux E2E Tests', () => {
   it('shows swagger', () => {
     let host = Cypress.env('host_webflux') ? Cypress.env('host_webflux') : 'localhost';
     cy.visit(`http://${host}:8081/webjars/swagger-ui/index.html`);
-    cy.get('h2').contains('OpenAPI definition').should('not.be.null');
+    cy.contains('OpenAPI definition').should('not.be.null');
     cy.wait(1000);
 
     cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8081');
